905706be5073e903913f9080917ca7e1c6841f66
mksir12 <116490353+mksir12@users.noreply.github.com>
2023-05-29 23:24:30 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -205,7 +205,7 @@ def start(update: Update, context: CallbackContext):
         else:
             first_name = update.effective_user.first_name
             update.effective_message.reply_sticker(
-                "CAACAgUAAxkBAAJYsmLWRvm70cE-mmxSNCovEf4v1ueJAAIcCAACbMK4VuL4EmZEkq8WKQQ"
+                "CAACAgUAAxkBAAIC02R05q8ssqlGLG62h6-kM-Mp4JW9AAIrCQACZcyRVxXY4k0Jo6ooLwQ"
             )
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), BOT_NAME),
